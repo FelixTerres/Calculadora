b54aabf3f96586ba61d7751b2f6f7bf51c8e968b
FelixTerres <fterres20935@iesjoanramis.org>
2024-10-14 10:42:49 +0200

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,24 @@
+function suma() {
+    let n1 = parseFloat(prompt("Dime un numero"))
+    let n2 = parseFloat(prompt("Dime otro numero"))
+    document.write(n1 + n2)
+}
+function resta() {
+    let n1 = parseFloat(prompt("Dime un numero"))
+    let n2 = parseFloat(prompt("Dime otro numero"))
+    document.write(n1 - n2)
+}
+function multiplicar() {
+    let n1 = parseFloat(prompt("Dime un numero"))
+    let n2 = parseFloat(prompt("Dime otro numero"))
+    document.write(n1 * n2)
+}
+function dividir() {
+    let n1 = parseFloat(prompt("Dime un numero"))
+    let n2 = parseFloat(prompt("Dime otro numero"))
+    document.write(n1/n2)
+}
+function arrel() {
+    let n1 = parseFloat(prompt("Dime un numero"))
+    document.write(Math.sqrt(n1))
+}
